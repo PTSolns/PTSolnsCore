7ccddc74a1cfe77543c574415953ad6567e0bc31
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-12-21 13:51:57 +0100

    Revert "feat: ch340g drivers"
    This reverts commit af1de0f182630eece260482d1594fecef4d642bc.

--- avr-1.8.6/post_install.bat
+++ avr-1.8.6/post_install.bat
@@ -17,13 +17,4 @@ if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
   drivers\dpinst-x86.exe /PATH "%cd%\drivers\gemma" %ARGS%
 )
 
-REM dpinst /PATH has problems with relative paths, so use absolute path.
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\ch340g" %ARGS%
-) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\ch340g" %ARGS%
-) ELSE (
-  drivers\dpinst-x86.exe /PATH "%cd%\drivers\ch340g" %ARGS%
-)
-
 exit /b 0
