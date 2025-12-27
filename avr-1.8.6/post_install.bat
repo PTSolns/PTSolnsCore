1c0f80d5b719fc2133f5bb22f1a5074a171076c3
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2025-12-27 13:40:32 +0100

    feat: Experimental removal of drivers

--- avr-1.8.6/post_install.bat
+++ avr-1.8.6/post_install.bat
@@ -1,20 +1,20 @@
 @echo off
-set ARGS=/LM /SW /SA
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  drivers\dpinst-amd64.exe %ARGS%
-) ELSE (
-  drivers\dpinst-x86.exe %ARGS%
-)
+@REM set ARGS=/LM /SW /SA
+@REM if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+@REM   drivers\dpinst-amd64.exe %ARGS%
+@REM ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+@REM   drivers\dpinst-amd64.exe %ARGS%
+@REM ) ELSE (
+@REM   drivers\dpinst-x86.exe %ARGS%
+@REM )
 
-REM dpinst /PATH has problems with relative paths, so use absolute path.
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
-) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
-  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
-) ELSE (
-  drivers\dpinst-x86.exe /PATH "%cd%\drivers\gemma" %ARGS%
-)
+@REM REM dpinst /PATH has problems with relative paths, so use absolute path.
+@REM if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+@REM   drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
+@REM ) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+@REM   drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
+@REM ) ELSE (
+@REM   drivers\dpinst-x86.exe /PATH "%cd%\drivers\gemma" %ARGS%
+@REM )
 
 exit /b 0
